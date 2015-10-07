5938d3e2323aae9c9d3df55f717cbb473499f25f
natashalynne <hakun.natasha@gmail.com>
2015-10-07 19:53:13 -0400

    Start writing jQuery for adding list item.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
 
+  $("button").click(function() {
+    var newItemContainer = $("<div></div>", {class: "item-container"});
+    $(".registered-inputs").append(newItemContainer);
+    var newCheckBox = $("<form></form>", {class: "checkbox"});
+    $(newItemContainer).append(newCheckBox);
+    var newCheckBoxInput = $("<input>", {type: "checkbox"});
+    $(newItemContainer).append(newCheckBoxInput);
+
+    var newItem = $("<div></div>", {class: "item"});
+    var item = $(".item-input").val();
+    $(newItem).append(item);
+    $(newItemContainer).append(newItem);
+
+  })
+});
